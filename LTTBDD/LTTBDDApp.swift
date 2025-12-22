3bbc81cb6be711e4626c35004c8bf1ed3965aca0
Quang <quangvm5761@ut.edu.vn>
2025-12-22 21:04:11 +0700

    Initial Commit

---
+++ LTTBDD/LTTBDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTTBDDApp.swift
+//  LTTBDD
+//
+//  Created by Quang Vũ on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LTTBDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
